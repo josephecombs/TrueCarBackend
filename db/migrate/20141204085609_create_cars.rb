367fecd2534595e11841ad879826ca07feaf4f8b
Joseph Combs <joseph.e.combs@gmail.com>
2014-12-04 19:19:42 -0800

    preparing to test regex matching and construction

--- db/migrate/20141204085609_create_cars.rb
+++ db/migrate/20141204085609_create_cars.rb
@@ -1,7 +1,7 @@
 class CreateCars < ActiveRecord::Migration
   def change
     create_table :cars do |t|
-      t.text :serial_number_pattern, null: false
+      t.text :serial_number_pattern, null: false, length: { is: 12 }
       t.integer :trim_id, null: false
       t.integer :year, null: false
       t.string :make, null: false
