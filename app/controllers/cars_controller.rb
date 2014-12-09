56b63ca8ac0ae477ea5ce5cbbdd204c7913fc92f
Joseph Combs <joseph.e.combs@gmail.com>
2014-12-08 16:50:09 -0800

    finished rails api, about to add client-side validation to search field

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,7 +5,8 @@ class CarsController < ApplicationController
       #TODO: check for bad query string in here with a regex, although it should not get to that point because we are using js to do the same thing for the user
       @cars = Car.regexed_search(params[:serial_number_pattern])
     else
-      @cars = Car.all
+      # @cars = Car.all
+      @cars = []
     end
     render :index
   end
--- app/models/car.rb
+++ app/models/car.rb
@@ -6,14 +6,43 @@ class Car < ActiveRecord::Base
   #TODO: has_many ... previous_owners? transactions?
   
   def self.regexed_search (string)
-    query_term = ""
     
-    string.each_char do |char|
-      query_term += '[' + char.downcase + '|\*]'
+    #this checking is really only needed for times when the user is directly typing in params to url bar
+    pure_string = string.gsub(/[^a-zA-Z0-9 ]/,"")
+    puts "1"
+    #invalid chars
+    if (pure_string.length == string.length)
+      puts "2"
+      #invalid length
+      if (pure_string.length == 12)
+        puts "3"
+        temp_letters = pure_string[0..5]
+        temp_letters = temp_letters.gsub(/[^a-zA-Z]/,"")
+        puts "aaa"
+        puts pure_string
+        puts temp_letters
+        #numbers in first 6 chars
+        if (temp_letters.length == 6)
+          puts "4"
+          temp_letters_2 = pure_string[6..11].gsub(/[^0-9]/,"")
+          #letters in last six chars
+          if (temp_letters_2.length == 6)
+            puts "5"
+            #finally construct query and return the values
+            query_term = ""
+    
+            string.each_char do |char|
+              query_term += '[' + char.downcase + '|\*]'
+            end
+            
+            #TODO: confirm that this way is not prone to sql injection
+            return Car.where('serial_number_pattern ~* ?', query_term)
+          end
+        end
+      end
     end
     
-    #TODO: confirm that this way is not prone to sql injection
-    Car.where('serial_number_pattern ~* ?', query_term)
+    return []  
     # Car.where('serial_number_pattern ~* ?', '[x|\*][x|\*][r|\*][c|\*][a|\*][v|\*][1|\*][2|\*][3|\*][4|\*][5|\*][6|\*]')
   end
 end
